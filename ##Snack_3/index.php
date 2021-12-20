9ed2e1626829a8e59f3940e7ff94cdb3ed6be013
ABoni14 <aboni150@gmail.com>
2021-12-20 16:22:45 +0100

    snack 3 finish

--- ##Snack_3/index.php
+++ ##Snack_3/index.php
@@ -1,4 +1,58 @@
 <?php 
+/*
+## Snack 3
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
+*/
+$posts= [
+
+  '10/01/2019' => [
+      [
+          'title' => 'Post 1',
+          'author' => 'Michele Papagni',
+          'text' => 'Testo post 1'
+      ],
+      [
+          'title' => 'Post 2',
+          'author' => 'Michele Papagni',
+          'text' => 'Testo post 2'
+      ],
+  ],
+  '10/02/2019' => [
+      [
+          'title' => 'Post 3',
+          'author' => 'Michele Papagni',
+          'text' => 'Testo post 3'
+      ]
+  ],
+  '15/05/2019' => [
+      [
+          'title' => 'Post 4',
+          'author' => 'Michele Papagni',
+          'text' => 'Testo post 4'
+      ],
+      [
+          'title' => 'Post 5',
+          'author' => 'Michele Papagni',
+          'text' => 'Testo post 5'
+      ],
+      [
+          'title' => 'Post 6',
+          'author' => 'Michele Papagni',
+          'text' => 'Testo post 6'
+      ]
+  ]
+];
+
+for($i = 0; $i < count(array_keys($posts)); $i++){
+  echo array_keys($posts)[$i];
+  echo "<br>";
+
+  foreach ($posts[array_keys($posts)[$i]] as $post){
+    echo $post["title"] . ":" . $post["author"] . " - " . $post["text"];
+    echo "<br>";
+  }
+  echo "<br>";
+};
 
 ?>
 
@@ -8,7 +62,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Snack 3</title>
 </head>
 <body>
   
